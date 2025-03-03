3f4f7a9e686c1e5cc216a02a3aa322b9511e4f57
DavideTamburello <davide.tamburello@issgreppi.it>
2025-03-03 11:44:25 +0100

    Add files via upload

---
+++ 03_03_2025/03_03_2025/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Dynamic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+    }
+
+    public interface ISocial
+    {
+        void Saluta();
+        void Segue(int numFollower, string social);
+        void Seguito(int numFollower, string social);
+    }
+    class Persona : ISocial
+    {
+        //fields
+        string Nome { get; set; }
+        string Cognome { get; set; }
+        DateTime DataNascita { get; set; }
+        public int Eta
+        {
+            get 
+            {
+                DateTime today = DateTime.Today;
+                int eta =  today.Year - DataNascita.Year;
+                if(DataNascita.Date > today.AddYears(-eta))
+                {
+                    eta--;
+                }
+                return eta;
+            }
+        }
+        public Persona(string nome, string cognome, DateTime dataNascita)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            DataNascita = dataNascita;
+        }
+        public Persona(string nome, string cognome)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            DataNascita = DateTime.Now;
+        }
+
+        public void ToString(Persona persona)
+        {
+            System.Console.WriteLine(persona);
+        }
+        public void MiPresento(Persona persona)
+        {
+            System.Console.WriteLine($"mi chiamo {Nome} {Cognome} e ho {Eta} anni");
+        }
+        public void Saluta()
+        {
+
+        }
+        public void Segue(int numFollower, string social)
+        {
+
+        }
+        public void Seguito(int numFollower, string social)
+        {
+
+        }
+    }
+}
