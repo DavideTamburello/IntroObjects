a5dd809c31d62581e306f2b950d24c538b1c7acb
DavideTamburello <davide.tamburello@issgreppi.it>
2025-03-04 08:59:44 +0100

    Add files via upload

---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,4 @@
+﻿//pag 836
+//polimorfismo = capacità delle reference di comportarsi in maniera differente a seconda dell'oggetto referenziale
+//per avere polimorfismo dobbiamo usare una reference di tipo base class e più oggetti di classi derivate
+//esempio: uso una reference di tipo felino per "puntare" (referenziare) oggetti di tipo Leone oppure di tipo Leopardo
