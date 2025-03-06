b50099b77708440b7162098621b5fde5242218e8
DavideTamburello <davide.tamburello@issgreppi.it>
2025-03-06 12:56:14 +0100

    Add files via upload

---
+++ 06_03_2025/06_03_2025/Program.cs
@@ -0,0 +1,38 @@
+﻿using _06_03_2025;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //coma facciamo a ordinare gli oggetti di tipo temperatura?
+        //creiamo un array di oggetti di tipo temperatura
+
+        const int NumeroDiTemperature = 10;
+        Random gen = new Random();
+        Temperatura[] temperature = new Temperatura[NumeroDiTemperature];
+
+        for(int i = 0; i <temperature.Length; i++)
+        {
+            temperature[i] = new Temperatura(gen.Next(-10, 41));
+        }
+        //stampiamo le temperature
+        PrintTemperature(temperature);
+        Array.Sort(temperature); //--> per funzionare richiede che gli oggetti nell'array implementino IComparable
+        Console.WriteLine("stampa delle temperature ordinate");
+        PrintTemperature(temperature);
+
+        //confronti con oggetti di tipo Temperatura
+        if (temperature[4] > temperature[3])
+        {
+
+        }
+    }
+
+    private static void PrintTemperature(Temperatura[] temperature)
+    {
+        foreach(var tempreratura in temperature)
+        {
+            Console.WriteLine($"temperatura = {temperature.Celsius:F2} °C, corrispondente a {temperature.Fahreneit:F2} °F");
+        }
+    }
+}
---
+++ 06_03_2025/06_03_2025/Temperatura.cs
@@ -0,0 +1,64 @@
+﻿namespace _06_03_2025
+//se si implementa IComparables<T> è possibile fare l'override degli operatori
+{
+    internal class Temperatura : IComparable, IComparable<Temperatura>
+    {
+        //internamente la classe Temperatura memorizza la temperatura in gradi
+        //Fahreneit
+        protected double temperaturaF;
+        public double Fahreneit
+        {
+            get
+            {
+                return temperaturaF;
+            }
+            set
+            {
+                temperaturaF = value;
+            }
+        }
+        
+        public double Celsius
+        {
+            get
+            {
+                return (temperaturaF - 32) * (5.0 / 9);
+            }
+            set
+            {
+                temperaturaF = (value*9.0/5)+32;
+            }
+        }
+        public Temperatura(double celsius)
+        {
+            Celsius = celsius;
+        }
+        public int CompareTo(object? obj)
+        {
+            if(obj == null) //oppure obj is null
+            {
+                return 1; //vuol dire che this (che è non nullo) è maggiore di null
+            }
+            Temperatura objTemperatura = obj as Temperatura;
+            if(objTemperatura is not null) //oppure objTemperatura != null
+            {
+                return temperaturaF.CompareTo(objTemperatura.temperaturaF);
+
+            }else //l'oggetto obj non è una temperatura
+            {
+                throw new ArgumentException("object non è una temperatura");
+            }
+        }
+
+        //versione tipizzata del CompareTo
+        public int CompareTo(Temperatura? other)
+        {
+            if(other == null)
+            {
+                return 1;
+            }
+            return temperaturaF.CompareTo(other.temperaturaF);
+        }
+
+    }
+}
