316f4a83dca4c7fbb9cb6fe0b264a3f6a9c7b29a
DavideTamburello <davide.tamburello@issgreppi.it>
2025-03-11 08:58:53 +0100

    Add files via upload

---
+++ 11_03_2025/11_03_2025/Box.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_03_2025
+{
+    internal class Box
+    {
+        double larghezza;
+        double profondità;
+        double altezza;
+
+        public Box(double larghezza, double profondità, double altezza)
+        {
+            this.larghezza = larghezza;
+            this.profondità = profondità;
+            this.altezza = altezza;
+        }
+
+        public double Larghezza { get => larghezza; set => larghezza = value; }
+        public double Profondità { get => profondità; set => profondità = value; }
+        public double Altezza { get => altezza; set => altezza = value; }
+
+        //due box sono uguali se hanno le stesse dimensioni
+        //other = l'altra box
+        //return = true se le due box hanno le stesse dimensioni
+        public bool Equals(Box? other)
+        {
+            if(other is null)
+            {
+                return false;
+            }
+            return larghezza == other.larghezza && profondità == other.profondità && altezza == other.altezza;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            //gestione casi nulli
+            if(obj is null)
+            {
+                return false;
+            }
+            //verifichiamo se obj è di tipo Box e nel caso verifico l'uguaglianza
+            //se obj non è Box restituisco false
+            if(obj is Box other)
+            {
+                return Equals(other);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            //utilizziamo un criterio coerente per la definizione dell'Equals e del GetHashCode
+            return HashCode.Combine(larghezza, profondità, altezza);
+        }
+
+        public static bool operator == (Box left, Box right)
+        {
+            //gestire i casi nulli
+            if(left is null)
+            {
+                return right is null;
+            }
+            //usare Equals per l'uguaglianza
+            return left.Equals(right);
+        }
+        public static bool operator != (Box left, Box right)
+        {
+            //gestire i casi nulli
+            //usare Equals per l'uguaglianza
+        }
+
+        public override string ToString()
+        {
+
+        }
+    }
+}
---
+++ 11_03_2025/11_03_2025/Program.cs
@@ -0,0 +1,15 @@
+﻿using _11_03_2025;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //testing di Box
+        Box b1 = new Box(2, 3, 4);
+        Box b2 = new Box(2, 3, 4);
+        if(b1 == b2)
+        {
+            Console.WriteLine("le due scatole sono uguali");
+        }
+    }
+}
