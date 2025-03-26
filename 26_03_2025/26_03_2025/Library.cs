2b77659e3367e32064de7a97be6d2b757eee98dd
DavideTamburello <davide.tamburello@issgreppi.it>
2025-03-26 13:51:40 +0100

    Add files via upload

---
+++ 26_03_2025/26_03_2025/Library.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+namespace _26_03_2025;
+public class Library : IEnumerable<Book>
+{
+    private List<Book> books = new List<Book>();
+
+    public void AddBook(Book book)
+    {
+        books.Add(book);
+    }
+
+    // Implementazione dell'iteratore di base per scorrere tutti i libri
+    public IEnumerator<Book> GetEnumerator()
+    {
+        foreach (var book in books)
+        {
+            yield return book;
+        }
+    }
+
+    // Implementazione esplicita per IEnumerable (non generico)
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    // ... (Altri metodi e iterator da inventare da mettere qui)
+    public IEnumerable<Book> GetBooksByAuthor(string author)
+    {
+        foreach (var book in books)
+        {
+            if (book.Author.ToLower() == author.ToLower())
+            {
+                yield return book;
+            }
+        }
+    }
+    public IEnumerable<Book> GetBooksPublishedAfterYear(int year)
+    {
+        foreach (var book in books)
+        {
+            if (book.PublicationYear > year)
+            {
+                yield return book;
+            }
+        }
+    }
+    public IEnumerable<string> GetBookTitles()
+    {
+        foreach (var book in books)
+        {
+            yield return book.Title;
+        }
+    }
+    public IEnumerable<Book> GetBooksByYear(Library libreria, int year)
+    {
+        foreach (var book in libreria)
+        {
+            if (book.PublicationYear == year)
+            {
+                yield return book;
+            }
+        }
+    }
+}
---
+++ 26_03_2025/26_03_2025/Program.cs
@@ -0,0 +1,50 @@
+﻿using _26_03_2025;
+using static System.Reflection.Metadata.BlobBuilder;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Book book1 = new Book("titolo1", "peppino", "isbn1", 2021);
+        Book book2 = new Book("titolo2", "peppino", "isbn2", 2022);
+        Book book3 = new Book("titolo3", "autore3", "isbn3", 2023);
+
+        Library libreria = new Library();
+        libreria.AddBook(book1);
+        libreria.AddBook(book2);
+        libreria.AddBook(book3);
+
+        var a = libreria.GetBooksPublishedAfterYear(2021);
+        Console.WriteLine("GetBooksPublishedAfterYear(2021)");
+        foreach (var book in a)
+        {
+            Console.WriteLine(book);
+        }
+        Console.WriteLine();
+
+        a = libreria.GetBooksByAuthor("peppino");
+        Console.WriteLine("GetBooksByAuthor(\"peppino\")");
+        foreach (var book in a)
+        {
+            Console.WriteLine(book);
+        }
+        Console.WriteLine();
+
+        var b = libreria.GetBookTitles();
+        Console.WriteLine("GetBookTitles()");
+        foreach (var book in b)
+        {
+            Console.WriteLine(book);
+        }
+        Console.WriteLine();
+
+        a = libreria.GetBooksByYear(libreria, 2022);
+        Console.WriteLine("GetBooksByYear(libreria, 2022)");
+        foreach (var book in a)
+        {
+            Console.WriteLine(book);
+        }
+        Console.WriteLine();
+    }
+    
+}
