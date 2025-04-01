51f8a5a18697c7051272acf085a98e727cea0495
DavideTamburello <davide.tamburello@issgreppi.it>
2025-04-01 08:39:22 +0200

    Add files via upload

---
+++ 01_04_2025/01_04_2025/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //esempio sbagliatp
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        for(int i = 1; i <= 200000; i++)
+        {
+            collector += i;
+        }
+        sw.Stop();
+        var elapsedMilliseconds = sw.ElapsedMilliseconds;
+        Console.WriteLine(HandleCollector.Substring(..1024));
+        Console.WriteLine($"la versione sbagliata della concatenazione impiega ElapsedMilliseconds");
+
+        //versione corretta - uso di StringBuilder
+        StringBuilder sb = new();
+        sb.Append("numbers: ");
+        for(int i = 1; i <= 200000; i++)
+        {
+            //collector += i
+            sb.Append(i);
+        }
+        Console.WriteLine(sb.ToString()[0, 1024]);
+        Console.WriteLine($"la versione sbagliata della concatenazione impiega {elapsedMilliseconds}ms");
+
+    }
+}
