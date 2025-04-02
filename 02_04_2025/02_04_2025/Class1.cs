ed3d033fcfd5117f814569727a7e54b145535196
DavideTamburello <davide.tamburello@issgreppi.it>
2025-04-02 08:56:23 +0200

    Add files via upload

---
+++ 02_04_2025/02_04_2025/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Rubrica;
+
+public class Contact : IEquatable<Contact>
+{
+    public string Nome { get; set; }
+    public string NumeroTelefono { get; set; }
+    public string Email { get; set; }
+
+    public Contact(string nome, string numeroTelefono, string email)
+    {
+        Nome = nome;
+        NumeroTelefono = numeroTelefono;
+        Email = email;
+    }
+
+    override public string ToString()
+    {
+        return $"{nameof(Nome)}: {Nome}, {nameof(NumeroTelefono)}: {NumeroTelefono}, {nameof(Email)}: {Email}";
+    }
+
+    public bool Equals(Contact? other)
+    {
+        if(other is null)
+        {
+            return false;
+        }
+        return Nome == other.Nome && NumeroTelefono == other.NumeroTelefono && Email == other.Email;
+    }
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as Contact);
+    }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Nome, NumeroTelefono, Email);
+    }
+}
---
+++ 02_04_2025/02_04_2025/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Rubrica;
+
+public class ContactBook : IEnumerable<Contact>
+{
+    Dictionary<string, Contact> nome = new Dictionary<string, Contact>();
+    Dictionary<string, Contact> numero = new Dictionary<string, Contact>();
+    Dictionary<string, Contact> email = new Dictionary<string, Contact>();
+    List<Contact> contatti = new List<Contact>();
+
+    public void AddContact(Contact contatto)
+    {
+        nome.Add(contatto.Nome, contatto);
+        numero.Add(contatto.NumeroTelefono, contatto);
+        email.Add(contatto.Email, contatto);
+        contatti.Add(contatto);
+    }
+    public Contact? SearchContact(string searchTerm)
+    {
+        foreach (var contatto in nome)
+        {
+            if (contatto.Key == searchTerm)
+            {
+                return contatto.Value;
+            }
+        }
+        foreach (var contatto in numero)
+        {
+            if (contatto.Key == searchTerm)
+            {
+                return contatto.Value;
+            }
+        }
+        foreach (var contatto in email)
+        {
+            if (contatto.Key == searchTerm)
+            {
+                return contatto.Value;
+            }
+        }
+        return null;
+    }
+    public void ModifyContact(string nameToModify, Contact newContactInfo)
+    {
+        if(nome.ContainsKey(nameToModify))
+        {
+            string? emailDaEliminare;
+            string? numeroDaEliminare;
+            //elimino contatto corrispondente
+            if (nome[nameToModify].Email != null)
+            {
+                emailDaEliminare = nome[nameToModify].Email;
+                email.Remove(emailDaEliminare!);
+            }
+            if (numero[nameToModify].NumeroTelefono != null)
+            {
+                numeroDaEliminare = nome[nameToModify].NumeroTelefono;
+                email.Remove(numeroDaEliminare!);
+
+
+            }
+            contatti.Remove(newContactInfo);
+            nome.Remove(nameToModify);
+
+            //aggiungo il nuovo contatto
+            contatti.Add(newContactInfo);
+
+            if(newContactInfo.Email != null)
+            {
+                nome[newContactInfo.Email] = newContactInfo;
+            }
+            if (newContactInfo.Nome != null)
+            {
+                nome[newContactInfo.Nome] = newContactInfo;
+            }
+            if (newContactInfo.NumeroTelefono != null)
+            {
+                nome[newContactInfo.NumeroTelefono] = newContactInfo;
+            }
+        }
+    }
+    public void DeleteContact(string nameToDelete)
+    {
+        int i = 0;
+        foreach (var contatto in nome)
+        {
+            if (contatto.Key == nameToDelete)
+            {
+                nome.Remove(nameToDelete);
+                numero.Remove(nameToDelete);
+                email.Remove(nameToDelete);
+                contatti.RemoveAt(i);
+            }
+            i++;
+        }
+    }
+    // public string? PrintAllContacts()
+    // {
+    //     return contatti.ToString();
+    // }
+
+    public IEnumerator<Contact> GetEnumerator()
+    {
+        foreach (var contatto in contatti)
+        {
+            yield return contatto;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
