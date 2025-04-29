141028fd995a584626bbd0df361e78b3d00312d8
DavideTamburello <davide.tamburello@issgreppi.it>
2025-04-29 08:55:41 +0200

    Add files via upload

---
+++ 29_04_2025/29_04_2025/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string testo = "oggi analizzo del testo per vedere alcune proprietà";
+        StringBuilder sbLettere = new();
+        StringBuilder sbNumeri = new();
+        
+        foreach(char c in testo)
+        {
+            if(char.IsLetter(c))
+            {
+                sbLettere.Append(c);
+
+            }else if(char.IsDigit(c))
+            {
+                sbNumeri.Append(c);
+            }
+            //string di sole lettere
+            string soloLettere = sbLettere.ToString();
+            Console.WriteLine(soloLettere);
+            string soloNumeri = sbNumeri.ToString();
+            Console.WriteLine(soloNumeri);
+        }
+    }
+}
